f543177238952a456722ec2e8300878e2fbe7ad3
cboudereau <cboudereau@gmail.com>
2018-11-06 12:09:23 +0100

    fix entry point

--- legacylamp/docker-entrypoint.sh
+++ legacylamp/docker-entrypoint.sh
@@ -6,6 +6,9 @@ OLDPASSWORD=ChangeMe
 if [ "$1" = 'init' ]; then
     read -s -p "Enter a new password : " PASSWORD
     
+    sudo service apache2 restart
+    sudo service mysql restart
+
     mysql -uroot -p$OLDPASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASSWORD}';"
     echo $OLDPASSWORD | sudo -S -- echo hello sudo ! 
     echo -e "$PASSWORD\n$PASSWORD" | sudo passwd dev

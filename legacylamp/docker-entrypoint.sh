a74339f108386ddfa1a71fda45bb8e9600bd1fe5
cboudereau <cboudereau@gmail.com>
2018-11-06 15:44:59 +0100

    fix

--- legacylamp/docker-entrypoint.sh
+++ legacylamp/docker-entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 OLDPASSWORD=ChangeMe
 
 if [ "$1" = 'init' ]; then
-    read -s -p "Enter a new password : " PASSWORD
     
-    echo $OLDPASSWORD | sudo -S -- chown -R mysql /var/lib/mysql && sudo chgrp -R mysql /var/lib/mysql
+    echo -e $OLDPASSWORD | sudo -S -- chown -R mysql /var/lib/mysql && sudo chgrp -R mysql /var/lib/mysql
     sudo service apache2 restart
     sudo service mysql restart
 
+    read -s -p "Enter a new password : " PASSWORD
     mysql -uroot -p$OLDPASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASSWORD}';"
     echo -e "$PASSWORD\n$PASSWORD" | sudo passwd dev
 fi

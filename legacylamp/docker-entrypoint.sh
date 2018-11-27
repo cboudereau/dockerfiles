b42144447c4493559ae5566a4492725450f8736d
cboudereau <cboudereau@gmail.com>
2018-11-27 10:29:47 +0100

    clean useless messages

--- legacylamp/docker-entrypoint.sh
+++ legacylamp/docker-entrypoint.sh
@@ -5,14 +5,16 @@ OLDPASSWORD=ChangeMe
 
 if [ "$1" = 'init' ]; then
     
-    echo -e $OLDPASSWORD | sudo -S -- chown -R mysql /var/lib/mysql && sudo chgrp -R mysql /var/lib/mysql
-    sudo service apache2 restart
-    sudo service mysql restart
+    echo -e $OLDPASSWORD | sudo -S -- chown -R mysql /var/lib/mysql > /dev/null 2>&1 
+    sudo chgrp -R mysql /var/lib/mysql > /dev/null 2>&1
+    sudo service apache2 restart > /dev/null 2>&1
+    sudo service mysql restart > /dev/null 2>&1
 
-    read -s -p "Enter a new password : " PASSWORD
-    mysql -uroot -p$OLDPASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASSWORD}';"
-    echo -e "$PASSWORD\n$PASSWORD" | sudo passwd dev
+    read -s -p "Enter a new password : \n" PASSWORD
+    mysql -uroot -p$OLDPASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASSWORD}';" > /dev/null 2>&1
+    echo -e "$PASSWORD\n$PASSWORD" | sudo passwd dev > /dev/null 2>&1
 
+    echo 'Start the container to finish the setup'
     exit
 fi
 

7326260b450a6e52cc7971eb7c96b5420eee5ec3
cboudereau <cboudereau@gmail.com>
2018-11-28 14:05:03 +0100

    fix init script

--- legacylamp/docker-entrypoint.sh
+++ legacylamp/docker-entrypoint.sh
@@ -2,9 +2,10 @@
 set -e
 
 OLDPASSWORD=ChangeMe
+FIRST_RUN=~/.container_initialized
 
-if [ "$1" = 'init' ]; then
-    
+if [ ! -f $FIRST_RUN ]; then
+     
     echo -e $OLDPASSWORD | sudo -S -- chown -R mysql /var/lib/mysql > /dev/null 2>&1 
     sudo chgrp -R mysql /var/lib/mysql > /dev/null 2>&1
     sudo service apache2 restart > /dev/null 2>&1
@@ -14,7 +15,11 @@ if [ "$1" = 'init' ]; then
     mysql -uroot -p$OLDPASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${PASSWORD}';" > /dev/null 2>&1
     echo -e "$PASSWORD\n$PASSWORD" | sudo passwd dev > /dev/null 2>&1
 
-    echo -e '\nContainer configuration finished\nYou can run bash with this command\ndocker exec -it $HOSTNAME /bin/bash'
+    echo -e '\nContainer configuration finished"
+    echo "You can now run this commands:'
+    echo "docker start $HOSTNAME /bin/bash"
+    echo "docker exec -it $HOSTNAME /bin/bash"
+    touch $FIRST_RUN
     exit
 fi
 
